beba3b7862434a202342df28a79cc728f9565cac
PM Likhith Kumar <pmlikhithkumar055@gmail.com>
2026-05-29 09:55:50 +0000

    Added OpenLane config

---
+++ src/config.tcl
@@ -0,0 +1,10 @@
+set ::env(DESIGN_NAME) collision_detector
+
+set ::env(VERILOG_FILES) "\
+    /work/src/collision_detector.v"
+
+set ::env(CLOCK_PORT) ""
+set ::env(CLOCK_NET) ""
+
+set ::env(FP_CORE_UTIL) 20
+set ::env(PL_TARGET_DENSITY) 0.25
