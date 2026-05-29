2845d6df5b64dd3bb926b18621b9c88aecf14e23
PM Likhith Kumar <pmlikhithkumar055@gmail.com>
2026-05-29 17:38:18 +0000

    Fixed config syntax

--- src/config.tcl
+++ src/config.tcl
@@ -1,10 +1,9 @@
 set ::env(DESIGN_NAME) tt_um_v2x_warning
 
-set ::env(VERILOG_FILES) "\
-    /work/src/tt_um_v2x_warning.v
+set ::env(VERILOG_FILES) "/work/src/tt_um_v2x_warning.v"
 
-set ::env(CLOCK_PORT) ""
-set ::env(CLOCK_NET) ""
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_NET) "clk"
 
 set ::env(FP_CORE_UTIL) 20
 set ::env(PL_TARGET_DENSITY) 0.25
