eeca4b32ad380bb7d92a05d2019cfae503ebb698
PM Likhith Kumar <pmlikhithkumar055@gmail.com>
2026-05-29 10:34:13 +0000

    Added TinyTapeout compatible ports

--- src/tt_um_v2x_warning.v
+++ src/tt_um_v2x_warning.v
@@ -1,15 +1,33 @@
-module tt_um_v2x_warning(
-    input  [7:0] vehicle1_pos,
-    input  [7:0] vehicle2_pos,
-    output collision_warning
+module tt_um_v2x_warning (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
+wire [7:0] vehicle1_pos;
+wire [7:0] vehicle2_pos;
+wire collision_warning;
 wire [7:0] distance;
 
-assign distance = (vehicle1_pos > vehicle2_pos) ?
-                  (vehicle1_pos - vehicle2_pos) :
-                  (vehicle2_pos - vehicle1_pos);
+assign vehicle1_pos = ui_in;
+assign vehicle2_pos = uio_in;
 
-assign collision_warning = (distance < 8'd10) ? 1'b1 : 1'b0;
+assign distance =
+    (vehicle1_pos > vehicle2_pos) ?
+    (vehicle1_pos - vehicle2_pos) :
+    (vehicle2_pos - vehicle1_pos);
+
+assign collision_warning = (distance < 8'd10);
+
+assign uo_out = {7'b0000000, collision_warning};
+
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
 
 endmodule
